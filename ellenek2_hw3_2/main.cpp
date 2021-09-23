a6a88e8cbecc92ce0b536408ce9c47de052f3a43
ellenkirsh <ellenek2@illinois.edu>
2021-09-22 20:44:49 -0500

    Update main.cpp

--- ellenek2_hw3_2/main.cpp
+++ ellenek2_hw3_2/main.cpp
@@ -6,9 +6,29 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void squareNreverse(int a[], int size){
+    for (int start = 0, end = size-1; start<end; start++, end--){
+        swap(a[start], a[end]);
+    }
+    for(int i = 0; i < (size); i++){
+        a[i] = a[i] * a[i];
+    }
+}
+int main() {
+    int n;
+    cout << "Enter the length of the array: ";
+    cin >> n;
+    int a[n];
+    cout << "Enter the integer array: ";
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    squareNreverse(a,n);
+    cout << "Reverse Squared Array: \n";
+    for(int i = 0; i < n; i++){
+        cout << a[i] << endl;
+    }
     return 0;
 }
